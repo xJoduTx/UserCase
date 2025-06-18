aea980a12ae186c4360d3cdffced22596e19a352
Jodut <sashakalachev73@gmail.com>
2025-06-19 00:20:26 +0300

    v2 добавил минималку для RabbitMQ, теперь можно закидывать сообщения в модерку RabbitMQ

--- src/main/java/com/apiVerse/UserCase/controller/MainController.java
+++ src/main/java/com/apiVerse/UserCase/controller/MainController.java
@@ -1,4 +1,27 @@
 package com.apiVerse.UserCase.controller;
 
+import com.apiVerse.UserCase.service.MessageSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
+
+    @Autowired
+    private MessageSender messageSender;
+
+    @PostMapping("/send")
+    public ResponseEntity<?> send(@RequestBody String text) {
+        if (text.isBlank()) {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(400));
+        }
+        messageSender.send(text);
+        return new ResponseEntity<>(HttpStatusCode.valueOf(200));
+    }
+
 }
